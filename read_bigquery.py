862bffe62e62063fd4269c7301edb5ee9d59e86d
lilf4p <leostoppa999@gmail.com>
2021-10-09 20:19:55 +0200

    scrivi i risultati su file csv

--- read_bigquery.py
+++ read_bigquery.py
@@ -1,6 +1,7 @@
 from google.cloud import bigquery
 from google.oauth2 import service_account
 import decode
+import csv
 
 credentials = service_account.Credentials.from_service_account_file(
 '/home/lilf4p/Documenti/unipi/tesi-analisi-token/ethereum-blockchain-326613-b9f1ed7669a2.json')
@@ -9,15 +10,28 @@ project_id = 'ethereum-blockchain-326613'
 client = bigquery.Client(credentials= credentials,project=project_id)
 
 query_job = client.query("""
-   SELECT to_address, input
-   FROM token_erc20.trx_token10
+   SELECT address, from_address, to_address, input, block_timestamp 
+   FROM token_erc20.trx_10token
    LIMIT 100 """)
 
 results = query_job.result() #aspetta che venga completata la query
 
-list_res = [] #lista di coppie (funzione,parametri)
-for result in results:
-    func_obj,func_params = decode.start(result[0],result[1])
-    list_res.append((func_obj,func_params))
+#decodifica input
+#list_res = [] #lista di coppie (funzione,parametri)
+#for result in results:
+#    func_obj,func_params = decode.start(result[0],result[1])
+#    list_res.append((func_obj,func_params))
+#print(list_res)
 
-print(list_res)
+#header csv
+fieldnames = ['address', 'from_address', 'to_address', 'input', 'block_timestamp']
+
+#apro csv in scrittura
+f = open('trx_token.csv','w', encoding='UTF8', newline='')
+writer = csv.DictWriter(f, fieldnames=fieldnames)
+writer.writeheader()
+
+#scrivo ogni record in una riga
+for result in results:
+    writer.writerow(result)
+    
