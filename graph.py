632bf76ccc14bf6451c0042c85205cd21ecf2dbf
lilf4p <leostoppa999@gmail.com>
2021-10-23 19:16:16 +0200

    prove di creazione grafi

--- graph.py
+++ graph.py
@@ -1,14 +1,17 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from networkx.drawing.layout import random_layout
 
-f = open('./trx_contract/trx_contract_1.csv',"r")
+f = open('./trx_contract/trx_contract_10.csv',"r")
 next(f,None)
 Graphtype = nx.Graph()
 G = nx.read_edgelist(f, delimiter=',', create_using=Graphtype, nodetype=int, data=(("vmin",str),("vmax",str),("vavg",str),("vsum",str),("mintime",str),("maxtime",str),("count",str),("type",str),))
 
-pos = nx.random_layout(G)
+#pos = nx.nx_agraph.graphviz_layout(G,prog="sfdp")
+pos = random_layout(G)
 nx.draw(G,pos)
-nx.draw_networkx_edge_labels(G, pos)
+nx.draw_networkx_labels(G, pos)
 #print(G)
 
+#nx.write_gexf(G, "test.gexf")
 plt.show()
