5db84e59f1ce85c1f2c2d6b1589cb1b9e8342a72
lilf4p <leostoppa999@gmail.com>
2021-11-25 17:22:32 +0100

    distr pesi grafo

--- distr_val.py
+++ distr_val.py
@@ -1,27 +1,53 @@
+from numpy import longlong
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib.colors as mcolors
+from operator import itemgetter
+import numpy as np
+from decimal import Decimal
 
 cname = {1:"USDT", 2:"MGC", 3:"LINK", 4:"WETH", 5:"EOS", 6:"BAT", 7:"OMG", 8:"CPCT", 9:"TRX", 10:"SHIB"}
 list_patch = []
 ax = None
 
+weight = 'val_avg' #CAMBIA PER CALCOLARE LA DISTRIBUZIONE DI WEIGHT DIVERSI  
 
-for color,n in zip(mcolors.TABLEAU_COLORS,range(1,11)):    
+for color,n in zip(mcolors.TABLEAU_COLORS,range(1,11)):
     fname = './trx_contract/trx_contract_'+str(n)+'.csv'
+
+    #CREO DATAFRAME E CONVERTO IN NUMERI RAPPRESENTABILI
     df = pd.read_csv(fname)
+    df[weight] = ['%E' % Decimal(v) for v in df[weight]]
+    df[weight]=df[weight].astype('float64')
     print(df)
-    dfg = df.groupby(['val_avg']).size().reset_index(name='counts')
-    #print(dfg['val_max'].tolist())
-    #print(dfg.dtypes)
-    #dfg.astype('int64')
-    #print(dfg.dtypes)
-    #dfg['val_avg'] = dfg['val_avg'] + 1
+    print(df.dtypes)
+
+    #CALCOLO LE OCCORRENZE    
+    dfg = df.groupby([weight]).size().reset_index(name='counts')
     print(dfg)
-    #print(dfg['counts'].tolist())
-    ax = dfg.plot (x='val_avg',ax=ax, marker='.')
-    #plt.plot(dfg['val_max'].tolist(), dfg['counts'].tolist())
+    print(dfg.dtypes)
+    
+    #NORMALIZZO MINMAX LA MISURA WEIGHT
+    #scaler = MinMaxScaler()
+    #dfg = pd.DataFrame(scaler.fit_transform(dfg),columns=['degree','counts'])
+    #dfg[weight] = (dfg[weight] - dfg[weight].min()) / (dfg[weight].max() - dfg[weight].min())
+    #print(dfg)
+
+    #CALCOLO LE FREQUENZE NORMALIZZATE CON CDF
+    #pdf
+    dfg['pdf'] = dfg['counts'] / sum(dfg['counts'])
+    #cdf
+    dfg['cdf'] = dfg['pdf'].cumsum()
+    dfg = dfg.reset_index()
+
+    #TRASLO LE MISURE SULL'ASSE X DI UNO A DESTRA PER POTER RAPPRESENTARE LO 0 IN LOGSCALE (0 -> 1)
+    dfg[weight] = dfg[weight] + np.float64('%E' % Decimal('1'))
+    print(dfg)
+
+    #PLOTTO IL DATAFRAME ELABORATO
+    ax = dfg.plot (x=weight,y='counts',ax=ax, marker='.')
+
     patch = mpatches.Patch(color=color, label=cname[n])
     list_patch.append(patch)
 
@@ -33,7 +59,10 @@ f.set_figwidth(10)
 plt.xticks(fontsize=12,weight='bold')
 plt.yticks(fontsize=12,weight='bold')
 plt.ylabel('FREQUENCY', fontsize=18,weight='bold')
-plt.xlabel('AVERAGE TOKEN TRANSFER',fontsize=18,weight='bold')
-plt.title('NODES DEGREE DISTRIBUTION',fontsize=18,weight='bold')
 plt.legend(fontsize=15,handles=list_patch)    
+
+#CAMBIA IN BASE A WEIGHT
+plt.xlabel('AVERAGE TOKEN TRANSFER',fontsize=18,weight='bold') 
+plt.title('AVERAGE TOKEN TRANSFER DISTRUBUTION',fontsize=18,weight='bold')
 plt.savefig('./risultati_analisi/distr_avgtoken.png')
+
